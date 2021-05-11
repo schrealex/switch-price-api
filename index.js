359e86f72066b65666f57e655fe2163ebfc8daf6
Ceryl Bruitzman <ceryl.bruitzman@topicus.nl>
2021-05-11 10:58:58 +0200

    Minor changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running on ${port}, http://localhost:${port}`);
 });
 
 app.get('/get-game-price', (request, response) => {
